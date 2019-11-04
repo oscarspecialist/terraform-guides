d4511b8e52621c7584e2ccb9e4f126ef7cd3248d
oscarspecialist <52218498+oscarspecialist@users.noreply.github.com>
2019-11-04 15:31:56 -0600

    Remove tag block

--- self-serve-infrastructure/getting-started/terraform-aws/main.tf
+++ self-serve-infrastructure/getting-started/terraform-aws/main.tf
@@ -10,8 +10,4 @@ resource "aws_subnet" "demo_subnet" {
   vpc_id            = "${aws_vpc.demo_vpc.id}"
   cidr_block        = "${var.subnet_cidr_block}"
   availability_zone = "${var.subnet_availability_zone}"
-
-  tags {
-    Name = "fp_demo_subnet"
-  }
 }
